7fe39529c5b44df2c8113c2f261a012bb511d256
Djirgal <djirgal.nemgirova@gmail.com>
2021-12-02 04:01:16 +0300

    New activity activity_main_theme_choice.xml was added

---
+++ app/src/main/java/ru/learningandroid/calculator/MainActivityThemeChoice.java
@@ -0,0 +1,14 @@
+package ru.learningandroid.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivityThemeChoice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_theme_choice);
+    }
+}
